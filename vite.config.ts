af8a04e75df4b3c8e35f660890edf3f323ab2738
JLHSolutions <148152793+JLHSolutions@users.noreply.github.com>
2025-02-18 15:45:59 -0800

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-svg-loader'
+import Unocss from 'unocss/vite'
+import Components from 'unplugin-vue-components/vite'
+import { AnuComponentResolver } from 'anu-vue'
+
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        Unocss(),
+        svgLoader(),
+        Components({
+            resolvers: [
+                AnuComponentResolver()
+            ],
+            dts: true
+        })
+    ],
+
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true
+    },
+    resolve: {
+        alias: [
+            {
+                find: '@',
+                replacement: resolve(__dirname, './src')
+            }
+        ]
+    }
+})
